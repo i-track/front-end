c394de24bf65e83b7583cf6c5247ad522375a02b
Jerry_Sea <Jsea@kindsys.us>
2022-03-07 16:27:55 -0800

    added navbar and functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import axios from 'axios'
 import './App.css';
+import { Route, Link, Routes } from "react-router-dom";
+import { useState } from "react";
+import NavBar from './Components/NavBar/NavBar';
 
 function App() {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+const updateData = (isTrue) => {
+  setIsSubmitted(isTrue)
+}
+
+const resetIsSubmitted = ()=> {
+  setIsSubmitted(false)
+}
 
 
 
 
-
-
-  
   return (
     <div className="App">
+      <NavBar removeData={resetIsSubmitted} />
 
     </div>
   );
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,16 +1,48 @@
-import "./NavBar.css"
-import { SidebarData } from "./SideBarData";
+import "./NavBar.css";
+import { SideBarData } from "./SideBarData";
 import { Link } from "react-router-dom";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import { useState } from "react";
 
+const NavBar = ({ removeData }) => {
+  const [sidebar, setSidebar] = useState(false);
 
+  const showSidebar = () => setSidebar(!sidebar);
 
+  const handleQueryRemoval = () => {
+    removeData();
+  };
 
-const NavBar = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <div className="navbar">
+        <Link to="#" className="menu-bars">
+          <FaIcons.FaBars onClick={showSidebar} />
+        </Link>
+        <h2 className="logo">LOGO</h2>
+      </div>
+      <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+        <ul className="nav-menu-items" onClick={showSidebar}>
+          <li className="navbar-toggle">
+            <Link to="#" className="menu-bars">
+              <AiIcons.AiOutlineClose />
+            </Link>
+          </li>
+          {SideBarData.map((item, index) => {
+            return (
+              <li key={index} className={item.className}>
+                <Link to={item.path} onClick={handleQueryRemoval}>
+                  {item.icon}
+                  <span>{item.title}</span>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </>
+  );
+};
 
-export default NavBar
+export default NavBar;
