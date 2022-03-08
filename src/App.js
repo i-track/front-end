95988e0e1afc1d5fd17c18db72fa2c003a5fb72a
Jerry_Sea <Jsea@kindsys.us>
2022-03-08 14:51:44 -0800

    added post feature to the app.js Co-authored-by: Anthony <adkowalkowski@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 import "./App.css";
 import { Route, Link, Routes } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import NavBar from "./Components/NavBar/NavBar";
 import About from "./Components/About/About";
 
 function App() {
   const [isSubmitted, setIsSubmitted] = useState(false);
   const [departments, setDepartments] = useState([]);
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
 
   const updateData = (isTrue) => {
     setIsSubmitted(isTrue);
@@ -17,53 +19,50 @@ function App() {
     setIsSubmitted(false);
   };
 
-  const handleGet = () => {
+  // G E T
+
+  useEffect(() => {
     axios
-      .get("https://jsonplaceholder.typicode.com/posts?_limit=10", {
-        timeout: 5000,
-      })
+      .get("https://jsonplaceholder.typicode.com/posts")
       .then((res) => {
         setDepartments(res.data);
         console.log(res.data);
       })
       .catch((err) => console.log(err));
-  };
+  }, []);
 
-  const handlePost = () => {
-    axios
-      .post("https://jsonplaceholder.typicode.com/posts", {
-        userID: 1000,
-        title: "GROUP PROJECT",
-      })
-      .then((res) => console.log(res.data))
-      // .catch((err) => console.log(err));
-  };
+  // P O S T
 
-  // const handleDelete = (event) => {
-  //   let id = event.target.id;
-  //   let urlBase = "https://jsonplaceholder.typicode.com/posts"
-  //   let url = urlBase + "/" + id
-  //   fetch(url, {
-  //     method: "DELETE",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //   })
-  //   .then(res => res.json())
-  //   .then(d => console.log(d))
-  // };
-
-
-  const handleDelete = (id, e) => {
-    e.preventDefault();
-    axios.delete(`https://jsonplaceholder.typicode.com/posts/${id}`)
-    .then(res => console.log('Deleted', res).catch(err=> console.log(err)))
+  const postData = (e) => {
+    e.preventDefault()
+    axios.post('https://jsonplaceholder.typicode.com/posts', {
+      title,
+      body
+    }).then(res => console.log('Posting data', res)).catch(err => console.log(err))
   }
+
+  //  D E L E T E
+
+  const handleDelete = (id) => {
+    axios
+      .delete(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then((res) =>
+        console.log("Deleted", res).catch((err) => console.log(err))
+      );
+  };
+
+  // R E N D E R I N G  D A T A
 
   const departmentList = departments.map((department) => (
     <div className="departments-container">
-      <li key={department.id}>{department.title}</li>
-      <button onClick={(e) => handleDelete(department.id, e)}>Delete</button>
+      <table>
+        <tr>
+          <td style={{border: '1px solid black'}}>{department.id}</td>
+          <td style={{border: '1px solid black'}}>{department.title}</td>
+          <td style={{border: '1px solid black'}}>{department.body}</td>
+        </tr>
+      </table>
+      <button onClick={() => handleDelete(department.id)}>Delete</button>
     </div>
   ));
 
@@ -75,13 +74,29 @@ function App() {
           <Route path="/About" element={<About/>} />
         </Routes> */}
 
+      <form>
+        <label>Sign Up</label>
+        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+        <input type="text" value={body} onChange={(e) => setBody(e.target.value)} />
+        <hr/>
+        <button onClick={postData}>Post</button>
+      </form>
+
+
+
       {departmentList}
-      <button onClick={handleGet}>View Departments</button>
-      <button onClick={handlePost}>Post</button>
     </div>
   );
 }
 
 export default App;
 
-
+// const handlePost = () => {
+//   axios
+//     .post("https://jsonplaceholder.typicode.com/posts", {
+//       userID: 1000,
+//       title: "GROUP PROJECT",
+//     })
+//     .then((res) => console.log(res.data));
+//   // .catch((err) => console.log(err));
+// };
