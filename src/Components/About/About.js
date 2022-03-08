8b429ee6dffdacdb2d8a9adf059737a034bcb431
anthony <adkowalkowski@gmail.com>
2022-03-08 11:17:18 -0600

    add html to about component

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h1>ABOUT MEEE</h1>
+    <div className="about-me-container">
+      <h1>About us</h1>
+      <div className="goal-container">
+        <h2>Our Goal:</h2>
+        <p>
+          Our users' typically have the responsibility of managing large groups
+          of people. They can create departments and add or delete members so
+          that they can better understand the structure of their business.
+        </p>
+      </div>
+      <div className="developers-container">
+        <h1>Contributors:</h1>
+        <h2>Anthony Kowalkowski</h2>
+        <h2>Jerry Sea</h2>
+        <h2>Isaac Ruiz</h2>
+        <h2>KC. Anyalebechi</h2>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
