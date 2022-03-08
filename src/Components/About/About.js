59d1afbeeba6051af6fa5cbfc2fd1706e070f1b6
anthony <adkowalkowski@gmail.com>
2022-03-08 13:03:11 -0600

    modified our goal text

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -9,7 +9,7 @@ const About = () => {
       <div className="goal-container">
         <h2>Our Goal</h2>
         <p>
-          Our users' typically have the responsibility of managing large groups
+          Our users typically have the responsibility of managing large groups
           of people. They can create departments and add or delete members so
           that they can better understand the structure of their business.
         </p>
